ad5e8ea650fe6801f051fd78ae11e3b32044e71e
Sarthak Singh <ss269@uw.edu>
2023-02-27 13:19:02 +0530

    Fixed issues with google auth

--- src/client.rs
+++ src/client.rs
@@ -1,18 +1,20 @@
-use std::time;
-
+use jwt_compact::{
+    alg::{Rsa, RsaPrivateKey},
+    prelude::*,
+};
+use rsa::pkcs8::DecodePrivateKey;
 use worker::{Env, Request, Response};
 
 #[derive(Debug, Clone)]
 pub struct Token {
     pub access_token: String,
     pub token_type: String,
-    pub expiry: Option<time::Instant>,
+    pub expiry: Option<u64>,
 }
 
 pub struct Client {
-    client_id: String,
-    client_secret: String,
-    refresh_token: String,
+    private_key: String,
+    service_email_id: String,
 
     token: Option<Token>,
 }
@@ -22,9 +24,8 @@ impl Client {
 
     pub fn new(env: &Env) -> worker::Result<Self> {
         Ok(Client {
-            client_id: env.secret("GCP_CLIENT_ID")?.to_string(),
-            client_secret: env.secret("GCP_CLIENT_SECRET")?.to_string(),
-            refresh_token: env.secret("GCP_CLIENT_REFRESH_TOKEN")?.to_string(),
+            private_key: env.secret("GCP_PRIVATE_KEY")?.to_string(),
+            service_email_id: env.secret("GCP_SERVICE_EMAIL_ID")?.to_string(),
             token: None,
         })
     }
@@ -32,7 +33,7 @@ impl Client {
     async fn get_token(&mut self) -> worker::Result<Token> {
         if let Some(token) = self.token.clone() {
             if let Some(expiry) = token.expiry {
-                if expiry < time::Instant::now() {
+                if expiry < worker::Date::now().as_millis() {
                     self.token = None;
                 } else {
                     return Ok(token);
@@ -40,20 +41,42 @@ impl Client {
             }
         }
 
+        let key = RsaPrivateKey::from_pkcs8_pem(&self.private_key).unwrap();
+        let header = Header::default().with_token_type("jwt");
+
+        #[derive(serde::Serialize, serde::Deserialize)]
+        struct CustomClaims {
+            iss: String,
+            scope: String,
+            aud: String,
+        }
+        let claims = CustomClaims {
+            iss: self.service_email_id.clone(),
+            scope: "https://www.googleapis.com/auth/devstorage.full_control".into(),
+            aud: "https://oauth2.googleapis.com/token".into(),
+        };
+        let claims = Claims::new(claims)
+            .set_duration_and_issuance(&TimeOptions::default(), chrono::Duration::hours(1));
+
+        let token = Rsa::rs256()
+            .token(header, &claims, &key)
+            .expect("Failed to create jwt");
+
+        let mut url = url::Url::parse(Self::TOKEN_URL).expect("Failed to parse google token url");
+        {
+            let mut pairs = url.query_pairs_mut();
+            pairs.append_pair("grant_type", "urn:ietf:params:oauth:grant-type:jwt-bearer");
+            pairs.append_pair("assertion", token.as_str());
+        }
+
+        let mut headers = worker::Headers::default();
+        headers.append("Content-Type", "application/x-www-form-urlencoded")?;
+
         let req = Request::new_with_init(
-            Self::TOKEN_URL,
+            url.as_str(),
             &worker::RequestInit {
-                body: Some(
-                    serde_json::json!({
-                        "client_id": &self.client_id,
-                        "client_secret": &self.client_secret,
-                        "grant_type": "refresh_token",
-                        "refresh_token": &self.refresh_token
-                    })
-                    .as_str()
-                    .into(),
-                ),
-                headers: worker::Headers::default(),
+                body: None,
+                headers,
                 cf: worker::CfProperties::default(),
                 method: worker::Method::Post,
                 redirect: worker::RequestRedirect::default(),
@@ -69,14 +92,14 @@ impl Client {
 
         impl From<InternalToken> for Token {
             fn from(internal: InternalToken) -> Self {
-                let now = time::Instant::now();
+                let now = worker::Date::now();
 
                 Token {
                     access_token: internal.access_token,
                     token_type: internal.token_type,
                     expiry: internal
                         .expires_in
-                        .map(|expires_in| now + time::Duration::from_secs(expires_in)),
+                        .map(|expires_in| now.as_millis() + expires_in * 1000),
                 }
             }
         }
--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,16 @@ mod client;
 use std::collections::HashMap;
 
 use url::Url;
-use worker::{Env, Headers, Request, RequestInit, Response};
+use worker::{console_log, Env, Headers, Request, RequestInit, Response};
 
-const BUCKET: &'static str = "test-bucket-the-first";
+const BUCKET: &'static str = "test-bucket-the-first/";
 
 #[worker::event(fetch)]
 async fn main(req: Request, env: Env, _ctx: worker::Context) -> worker::Result<Response> {
     let router = worker::Router::new();
+
+    #[cfg(feature = "console_error_panic_hook")]
+    console_error_panic_hook::set_once();
 
     router
         .get_async("/", |_req, _ctx| async {
@@ -44,7 +47,16 @@ async fn main(req: Request, env: Env, _ctx: worker::Context) -> worker::Result<R
                 redirect: worker::RequestRedirect::default(),
             };
 
-            client.request(url.as_str(), init).await
+            console_log!("{}", url.as_str());
+            let resp = client.request(url.as_str(), init).await?;
+
+            Response::from_bytes(
+                resp.headers()
+                    .get("location")?
+                    .expect("No location header was found")
+                    .as_bytes()
+                    .to_vec(),
+            )
         })
         .run(req, env)
         .await
